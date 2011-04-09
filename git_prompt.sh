8aed0a9c4267031034d855bacdd54f409ce0633d
yerv000 <yerv000@yahoo.com>
2011-04-08 17:00:35 -0700

    Initial working script

---
+++ git_prompt.sh
@@ -0,0 +1,43 @@
+function set_git_prompt {
+  local last_commit_in_unix_time
+  local now_in_unix_time
+  local tmp_flags
+  local flags
+  local seconds_since_last_commit
+  local minutes_since_last_commit
+  local branch
+  last_commit_in_unix_time=$(git log "HEAD" --pretty=format:%ct 2> /dev/null | sort | tail -n1)
+  now_in_unix_time=$(date +%s)
+  branch=$(git branch --no-color 2> /dev/null | grep '*' | sed 's/\*//g' | sed 's/ //g')
+  tmp_flags=$(git status --porcelain 2> /dev/null | cut -c1-2 | sed 's/ //g' | cut -c1 | sort | uniq)
+  flags="$(echo $tmp_flags | sed 's/ //g')"
+  if [ $last_commit_in_unix_time ]; then
+    seconds_since_last_commit=$(($now_in_unix_time - $last_commit_in_unix_time))
+    minutes_since_last_commit="$(($seconds_since_last_commit/60))"
+    if ((minutes_since_last_commit < 60)); then
+      minutes_since_last_commit="\[\e[0;32m\]${minutes_since_last_commit}m\[\e[0m\]|"
+    elif ((minutes_since_last_commit < 120)); then
+      minutes_since_last_commit="\[\e[0;33m\]${minutes_since_last_commit}m\[\e[0m\]|"
+    else
+      minutes_since_last_commit="\[\e[0;31m\]${minutes_since_last_commit}m\[\e[0m\]|"
+    fi
+  else
+    minutes_since_last_commit=""
+  fi
+  if [ $branch ] || [ $flags  ]; then
+    if [ $branch ]; then
+      branch="\[\e[0;36m\]${branch}\[\e[0m\]"
+    else
+      branch="\[\e[0;36m\]waiting for first commit\[\e[0m\]"
+    fi
+    if [ $flags ]; then
+      PS1="${ORIGINAL_PS1/\\$ /}(${minutes_since_last_commit}${branch}|\[\e[0;35m\]${flags}\[\e[0m\])\$ "
+    else
+      PS1="${ORIGINAL_PS1/\\$ /}(${minutes_since_last_commit}${branch})\$ "
+    fi
+  else
+    PS1=$ORIGINAL_PS1
+  fi
+}
+ORIGINAL_PS1=$PS1
+PROMPT_COMMAND=set_git_prompt
